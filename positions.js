4a24b4cde74c67b91a94375aa38f8cb36265dce1
Cristopher Benavides <rashier13@gmail.com>
2022-10-22 23:38:59 +0200

    🌱 add positions.js file seed

---
+++ positions.js
@@ -0,0 +1,23 @@
+let positions = []
+  
+positions["5050"] = { "position": "1"},
+positions["25050"] = { "position": "2"},
+positions["45050"] = { "position": "3"},
+positions["50250"] = { "position": "4"},
+positions["250250"] = { "position": "5"},
+positions["450250"] = { "position": "6"},
+positions["50450"] = { "position": "7"},
+positions["250450"] = { "position": "8"},
+positions["450450"] = { "position": "9"},
+positions["100100"] = { "position": "1"},
+positions["300100"] = { "position": "2"},
+positions["500100"] = { "position": "3"},
+positions["100300"] = { "position": "4"},
+positions["300300"] = { "position": "5"},
+positions["500300"] = { "position": "6"},
+positions["100500"] = { "position": "7"},
+positions["300500"] = { "position": "8"},
+positions["500500"] = { "position": "9"}
+
+
+export default positions;
